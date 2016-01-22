fcd3a68914807476f6656e22ea3b39493d4f5950
Integralist <mark.mcdx@gmail.com>
2016-01-22 09:11:28 +0000

    Modification to work with both Alephant loggers CloudWatch AND Statsd

--- lib/mozart/logger.rb
+++ lib/mozart/logger.rb
@@ -6,27 +6,36 @@ require "bbc/cosmos/config"
 
 module Mozart
   module Logger
-    def self.setup(cloudwatch_opts, log_path)
-      Alephant::Logger.setup drivers(cloudwatch_opts, log_path)
+    def self.setup(opts, log_path)
+      Alephant::Logger.setup drivers(opts, log_path)
     end
 
-    def self.drivers(cloudwatch_opts, log_path)
+    def self.drivers(opts, log_path)
       [].tap do |drivers|
         drivers << json_driver(log_path)
-        drivers << cloudwatch_driver(cloudwatch_opts) if production?
+        drivers << metric_driver(opts)
       end
+    end
+
+    def self.metric_driver(opts)
+      return statsd_driver(opts[:statsd])  if opts[:statsd]     && production?
+      cloudwatch_driver(opts[:cloudwatch]) if opts[:cloudwatch] && production?
     end
 
     def self.json_driver(log_path)
       Alephant::Logger::JSON.new log_path
     end
 
-    def self.cloudwatch_driver(cloudwatch_opts)
-      Alephant::Logger::CloudWatch.new cloudwatch_namespace(cloudwatch_opts)
+    def self.statsd_driver(opts)
+      Alephant::Logger::Statsd.new opts
     end
 
-    def self.cloudwatch_namespace(cloudwatch_opts)
-      cloudwatch_opts.tap do |h|
+    def self.cloudwatch_driver(opts)
+      Alephant::Logger::CloudWatch.new cloudwatch_namespace(opts)
+    end
+
+    def self.cloudwatch_namespace(opts)
+      opts.tap do |h|
         h[:namespace] = h[:namespace] % BBC::Cosmos::Config.cosmos.environment
       end
     end
