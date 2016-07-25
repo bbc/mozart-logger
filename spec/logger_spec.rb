aa4ef639ca116145c9a102a547fc686638d99219
Darren Oakley <daz.oakley@gmail.com>
2016-07-25 12:21:40 +0100

    Run rubocop over the codebase

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,34 +1,38 @@
-require "pry"
-require "mozart/logger"
-require "bbc/cosmos/config"
+require 'pry'
+require 'mozart/logger'
+require 'bbc/cosmos/config'
 
 describe Mozart::Logger do
-  let(:opts)     {{
-    :statsd     => {},
-    :cloudwatch => {}
-  }}
-
-  let(:cloudwatch) {{
-    :cloudwatch => {
-      :namespace => "BBCApp/mozart-foo/%s"
+  let(:opts)     do
+    {
+      statsd:     {},
+      cloudwatch: {}
     }
-  }}
+  end
 
-  let(:log_path) { "app.log" }
+  let(:cloudwatch) do
+    {
+      cloudwatch: {
+        namespace: 'BBCApp/mozart-foo/%s'
+      }
+    }
+  end
+
+  let(:log_path) { 'app.log' }
 
   def enable_live
     allow(BBC::Cosmos::Config.cosmos)
-      .to receive(:environment).and_return("live")
+      .to receive(:environment).and_return('live')
   end
 
-  describe ".setup" do
+  describe '.setup' do
     specify do
-      expect(subject.setup opts, log_path).to be_a Alephant::Logger::Base
+      expect(subject.setup(opts, log_path)).to be_a Alephant::Logger::Base
     end
   end
 
-  describe ".drivers" do
-    context "Statsd and Cloudwatch provided" do
+  describe '.drivers' do
+    context 'Statsd and Cloudwatch provided' do
       specify do
         enable_live
         expect(subject.drivers(opts, log_path).length).to eq(2)
@@ -47,7 +51,7 @@ describe Mozart::Logger do
       end
     end
 
-    context "Only Cloudwatch provided" do
+    context 'Only Cloudwatch provided' do
       specify do
         expect(subject.drivers(cloudwatch, log_path).length).to eq(2)
       end
